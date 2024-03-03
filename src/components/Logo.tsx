c50dcc99c75cdab210e0634fd125cdc5805705aa
Munyangeyoelie <munyangeyoelie38@gmail.com>
2024-03-03 23:12:06 +0300

    error hundlering on log export

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,10 +2,7 @@ import React from "react";
 import Image from "next/image";
 
 const Logo = () => {
-  // Use a static import for the image file
-  const logoImage = require("../../public/logo");
-
-  return <Image src="/logo" alt="Your Logo Title" className="logo" />;
+  return <Image src="/logo.jpg" alt="logo of website" />;
 };
 
 export default Logo;
